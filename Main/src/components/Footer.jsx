96d3977add3d8cc8a63952ae49174ca21ca37e23
Colton Doling <cmdoling@gmail.com>
2024-12-07 17:49:08 -0500

    created a footer with images for github and linkedin and a link to github

---
+++ Main/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+// TODO: Display text or icon links to my GitHub, LinkedIn, and a third platform.
+function Footer() {
+    return (
+        <div>
+            <a href='https://github.com/Colton-Dol' target='_blank'><img src='./src/assets/images/github-mark-white.png' /></a>
+            <a href=''><img src='./src/assets/images/In-White-96-Γö¼┬½@2x.png' /></a>
+            <a href=''><img src='./src/assets/images/' /></a>
+        </div>
+    );
+}
+
+export default Footer;
