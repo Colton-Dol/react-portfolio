f528d3bb3fd04f6fe5d93673bab0543ef15bc0bd
Colton Doling <cmdoling@gmail.com>
2024-12-07 14:44:39 -0500

    imported pages, created paths for them with react-router-dom, and rendered the paths

--- Main/src/main.jsx
+++ Main/src/main.jsx
@@ -1,7 +1,39 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import './index.css';
+
+import App from './App.jsx';
+import AboutMe from './pages/AboutMe.jsx';
+import Contact from './pages/Contact.jsx';
+import Portfolio from './pages/Portfolio.jsx';
+import Resume from './pages/Resume.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <AboutMe />
+      },
+      {
+        path: '/Contact',
+        element: <Contact />
+      },
+      {
+        path: '/Portfolio',
+        element: <Portfolio />
+      },
+      {
+        path: '/Resume',
+        element: <Resume />
+      }
+    ]
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
-    <App />
-)
+    <RouterProvider router={router} />
+);
