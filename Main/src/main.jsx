e009557ae668cbf0ddfd0106a7dacff81c9617bb
Colton Doling <cmdoling@gmail.com>
2024-12-07 17:51:38 -0500

    added an errorElement to the router

--- Main/src/main.jsx
+++ Main/src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
+//import './index.css';
 
 import App from './App.jsx';
 import AboutMe from './pages/AboutMe.jsx';
@@ -14,6 +14,7 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    errorElement: <Error />,
     children: [
       {
         index: true,
