a2cfda382a5fb641c79dcb5c76009cafeece5ef9
Colton Doling <cmdoling@gmail.com>
2024-12-07 17:17:56 -0500

    created a basic structure for the About Me page

---
+++ Main/src/pages/AboutMe.jsx
@@ -0,0 +1,14 @@
+// TODO: Display a picture of myself and a short bio.
+function AboutMe() {
+return ( 
+<div>
+    <h1>About Me</h1>
+    <br />
+    <img src='./src/assets/images/Profile-Picture.jpg' className="rounded-circle w-25" alt="picture of a man with red hair wearing a green shirt, bucket hat, and glasses smiling at the camera" />
+    <br />
+    <p>Here is a paragraph about my life</p>
+</div>
+)
+}
+
+export default AboutMe;
