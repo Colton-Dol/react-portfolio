a74c04f87b605952f841ada07252e2d270a53166
Colton Doling <cmdoling@gmail.com>
2024-12-08 18:47:57 -0500

    added a function to handle empty or invalid fields in the form

--- Main/src/components/Form.jsx
+++ Main/src/components/Form.jsx
@@ -5,6 +5,7 @@ function Form() {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
+    const [error, setError] = useState('');
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -26,39 +27,65 @@ function Form() {
         setMessage('');
     }
 
+    const handleEmptyFields = () => {
+        const pattern = /\S+@\S+\.\S+/
+        
+        if (name === '') {
+            setError('Name is required');
+            return false;
+        } else if (email === '') {
+            setError('Email is required');
+            return false;
+        } else if (!pattern.test(email)) {
+            setError('Your email is invalid');
+            return false;
+        } else if (message === '') {
+            setError('Message is required');
+            return false;
+        } else {
+            setError('');
+        }
+
+        return true;
+    }
+
     return (
         <div>
             <form className="ms-5" onSubmit={handleSubmit}>
                 <div className="mb-3 w-50">
-                    <label for="name" className="form-label text-light">Name:</label>
+                    <label htmlFor="name" className="form-label text-light">Name:</label>
                     <input 
                         value={name}
                         name="name"
                         onChange={handleChange}
+                        onBlur={handleEmptyFields}
                         type="text"
                         className="form-control"
                     />
                 </div>
                 <div className="mb-3 w-50">
-                    <label for="email" className="form-label text-light">Email:</label>
+                    <label htmlFor="email" className="form-label text-light">Email:</label>
                     <input 
                         value={email}
                         name="email"
                         onChange={handleChange}
+                        onBlur={handleEmptyFields}
                         type="email"
                         className="form-control"
                     />
                 </div>
                 <div className="mb-3 w-50">
-                    <label for="message" className="form-label text-light">Message:</label>
+                    <label htmlFor="message" className="form-label text-light">Message:</label>
                     <textarea 
                         value={message}
                         name="message"
                         onChange={handleChange}
+                        onBlur={handleEmptyFields}
                         className="form-control"
                         rows={10}
                     />
                 </div>
+                <p className="mb-3 text-light">{error}</p>
                 <button type="submit" className="mb-5">
                     Submit
                 </button>
