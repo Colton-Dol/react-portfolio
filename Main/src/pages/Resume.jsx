184cf410aeb46c55750121259ec8adda542f2859
Colton Doling <cmdoling@gmail.com>
2024-12-09 13:57:22 -0500

    added some styling to Resume page

--- Main/src/pages/Resume.jsx
+++ Main/src/pages/Resume.jsx
@@ -1,20 +1,20 @@
 // TODO: Add a link to a downloadable resume and list front-end and back-end proficiecies
 function Resume() {
     return (
-        <div>
-            <h2>Resume</h2>
+        <div className="text-light ms-5 mt-5">
+            <h2 className="ms-5">Resume</h2>
             <br />
-            <p>Download my <a href="./src/assets/" download>resume</a></p>
+            <p className="ms-5">Download my <a href="./src/assets/" className="link-underline-opacity-0 link-secondary" download>resume</a></p>
             <br />
-            <h3>Front-end Proficiencies</h3>
-            <ul>
+            <h3 className="ms-5">Front-end Proficiencies</h3>
+            <ul className="p-4 ms-5">
                 <li>HTML</li>
                 <li>CSS</li>
                 <li>JavaScript</li>
                 <li>Bootstrap</li>
             </ul>
-            <h3>Back-end Proficiencies</h3>
-            <ul>
+            <h3 className="ms-5">Back-end Proficiencies</h3>
+            <ul className="p-4 ms-5">
                 <li>APIs</li>
                 <li>Node</li>
                 <li>Express</li>
