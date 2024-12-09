7e357ecab7ef08fbe7b78eccf00e2dd38797ca64
Colton Doling <cmdoling@gmail.com>
2024-12-09 15:28:38 -0500

    created basic portfolio page

---
+++ Main/src/pages/Portfolio.jsx
@@ -0,0 +1,7 @@
+// TODO: Implement the Project components in two columns side-by-side. Use props to make each Project different.
+import Project from '../components/Project'
+
+function Portfolio() {
+    return <Project />
+}
+export default Portfolio;
