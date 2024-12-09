93ef75f13e5145fd393e4e30d0daaca3e0807a44
Colton Doling <cmdoling@gmail.com>
2024-12-09 14:44:17 -0500

    imported a style sheet and did some bootstrap styling

--- Main/src/components/Footer.jsx
+++ Main/src/components/Footer.jsx
@@ -1,10 +1,16 @@
 // TODO: Display text or icon links to my GitHub, LinkedIn, and a third platform.
+import './Footer.css';
+
 function Footer() {
+const style = {
+    background: '#004346'
+}
+
     return (
-        <div>
-            <a href='https://github.com/Colton-Dol' target='_blank'><img src='./src/assets/images/github-mark-white.png' /></a>
-            <a href=''><img src='./src/assets/images/In-White-96-Γö¼┬½@2x.png' /></a>
-            <a href=''><img src='./src/assets/images/' /></a>
+        <div className="container text-center mt-5" style={style}>
+            <a href='https://github.com/Colton-Dol' target='_blank'><img src='./src/assets/images/github-mark-white.png' className="github Icon p-5" /></a>
+            <a href='https://linkedin.com'><img src='./src/assets/images/In-White-72.png' className='Icon p-5'/></a>
+            <a href=''><img src='./src/assets/images/' className='Icon p-5' /></a>
         </div>
     );
 }
