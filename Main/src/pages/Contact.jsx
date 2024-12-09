26e14444ce8fed4f3985ce88cdcd957390c1d7e1
Colton Doling <cmdoling@gmail.com>
2024-12-09 15:27:57 -0500

    created and styled the Contact page

---
+++ Main/src/pages/Contact.jsx
@@ -0,0 +1,13 @@
+// TODO: Implement the Form component.
+import Form from "../components/Form";
+
+function Contact() {
+    return (
+        <div className="ms-5 mt-5">
+            <h2 className="ms-5 pb-4 text-light">Contact</h2>
+            <Form />
+        </div>
+    )
+}
+
+export default Contact;
