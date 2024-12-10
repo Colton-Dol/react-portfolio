b3da37524e34e7a0874e92cc490e5395171939cc
Colton Doling <cmdoling@gmail.com>
2024-12-10 17:24:24 -0500

    edited the Footer component to use the new GitHub component, as well as changed the icon class to be lower case and removed the github class

--- Main/src/components/Footer.jsx
+++ Main/src/components/Footer.jsx
@@ -1,16 +1,15 @@
 // TODO: Display text or icon links to my GitHub, LinkedIn, and a third platform.
 import './Footer.css';
+import GitHub from './GitHub.jsx';
 
 function Footer() {
-const style = {
-    background: '#004346'
-}
+const link = 'https://github.com/Colton-Dol';
 
     return (
-        <div className="container text-center mt-5" style={style}>
-            <a href='https://github.com/Colton-Dol' target='_blank'><img src='./src/assets/images/github-mark-white.png' className="github Icon p-5" /></a>
-            <a href='https://linkedin.com'><img src='./src/assets/images/In-White-72.png' className='Icon p-5'/></a>
-            <a href=''><img src='./src/assets/images/' className='Icon p-5' /></a>
+        <div className="container text-center mt-5">
+            <GitHub link={link} />
+            <a href='https://linkedin.com'><img src='./src/assets/images/In-White-72.png' className='icon p-5'/></a>
+            <a href=''><img src='./src/assets/images/' className='icon p-5' /></a>
         </div>
     );
 }
