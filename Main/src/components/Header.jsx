36ae3fa9b46d24a2e76fc5cf19974e44378fbda3
Colton Doling <cmdoling@gmail.com>
2024-12-10 17:27:25 -0500

    changed the background image and added some bootstrap styling

--- Main/src/components/Header.jsx
+++ Main/src/components/Header.jsx
@@ -1,15 +1,17 @@
 // TODO: Have a background image with my name and implement the Navigation component.
-import Nav from './Navigation.jsx'
+import Nav from './Navigation.jsx';
 
 function Header() {
     const style ={
-        backgroundImage: `url("./src/assets/images/backgroundImage.avif")`
+        backgroundImage: `url("./src/assets/images/background.jpg")`
     }
 
     return (
-        <div className='p-5' style={style}>
-            <h1>Colton Doling</h1>
-            <Nav />
+        <div className='container-fluid p-5' style={style}>
+            <div className='row align-items-center'>
+                <h1 className='text-light'>Colton Doling</h1>
+                <Nav />
+            </div>
         </div>
     );
 }
