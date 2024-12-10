33b0892aadd3570cc550a12ca62f46f22bfed4cd
Colton Doling <cmdoling@gmail.com>
2024-12-10 17:28:36 -0500

    implemented six Project components and created test props for it

--- Main/src/pages/Portfolio.jsx
+++ Main/src/pages/Portfolio.jsx
@@ -1,7 +1,21 @@
 // TODO: Implement the Project components in two columns side-by-side. Use props to make each Project different.
-import Project from '../components/Project'
+import Project from '../components/Project';
 
 function Portfolio() {
-    return <Project />
-}
+    const title = 'Pizza Pizza';
+    const image = './src/assets/images/blue-background.jpg';
+    const app = 'https://littlecaesers.com';
+    const repo = 'https://github.com/Colton-Dol/employee-tracker';
+
+    return (
+        <div className='row ms-5 mt-5'>
+         <Project title={title} image={image} app={app} repo={repo} />
+         <Project />
+         <Project />
+         <Project />
+         <Project />
+         <Project />
+        </div>
+    )
+}       
 export default Portfolio;
