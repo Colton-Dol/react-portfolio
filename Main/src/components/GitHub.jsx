5eab8a5dee304e2c3644664fc2058e8b9677b3af
Colton Doling <cmdoling@gmail.com>
2024-12-10 17:25:24 -0500

    removed the github class

--- Main/src/components/GitHub.jsx
+++ Main/src/components/GitHub.jsx
@@ -1,5 +1,5 @@
 function GitHub(props) {
-    return <a href={props.link} target="_blank"><img src="./src/assets/images/github-mark-white.png" className="github icon p-5" /></a>
+    return <a href={props.link} target="_blank"><img src="./src/assets/images/github-mark-white.png" className="icon p-5" /></a>
 }
 
 export default GitHub;
