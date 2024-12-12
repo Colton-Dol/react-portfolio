469315f9102909b7b09750ab01b7bb807a4ff4fd
Colton Doling <cmdoling@gmail.com>
2024-12-11 21:21:59 -0500

    removed margin class from footer

--- Main/src/components/Footer.jsx
+++ Main/src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
 const link = 'https://github.com/Colton-Dol';
 
     return (
-        <div className="container text-center mt-5">
+        <div className="container text-center">
             <GitHub link={link} />
             <a href='https://linkedin.com'><img src='./src/assets/images/In-White-72.png' className='icon p-5'/></a>
             <a href=''><img src='./src/assets/images/' className='icon p-5' /></a>
