264e32cf215bc6259f1b93a6023e52202f314c8c
Colton Doling <cmdoling@gmail.com>
2024-12-12 19:55:50 -0500

    added the header to Portfolio page

--- Main/src/pages/Portfolio.jsx
+++ Main/src/pages/Portfolio.jsx
@@ -10,29 +10,33 @@ function Portfolio() {
     const image2 = 'Square-Profile-Picture-2.jpg'
 
     return (
-        <div className='container align-items-center text-center mt-5'>
-            <div className='row justify-content-center pb-5'>
-                <div className='col-5 me-5' id='1'>
-                    <Project title={title} image={image} app={app} repo={repo} />
+        <div className='mt-5'>
+            <h2 className="ms-5 pb-4 text-light">Portfolio</h2>
+            <br />
+            <div className='container align-items-center text-center mt-5'>
+                <div className='row justify-content-center pb-5'>
+                    <div className='col-5 me-5' id='1'>
+                        <Project title={title} image={image} app={app} repo={repo} />
+                    </div>
+                    <div className='col-5 ms-5' id='2'>
+                        <Project title={title} image={image2} app={app} repo={repo} />
+                    </div>
                 </div>
-                <div className='col-5 ms-5' id='2'>
-                    <Project title={title} image={image2} app={app} repo={repo} />
+                <div className='row justify-content-center pb-5'>
+                    <div className='col-5 me-5' id='3'>
+                        <Project title={title} image={image} app={app} repo={repo} />
+                    </div>
+                    <div className='col-5 ms-5' id='4'>
+                        <Project title={title} image={image} app={app} repo={repo} />
+                    </div>
                 </div>
-            </div>
-            <div className='row justify-content-center pb-5'>
-                <div className='col-5 me-5' id='3'>
-                    <Project title={title} image={image} app={app} repo={repo} />
-                </div>
-                <div className='col-5 ms-5' id='4'>
-                    <Project title={title} image={image} app={app} repo={repo} />
-                </div>
-            </div>
-            <div className='row justify-content-center pb-5'>
-                <div className='col-5 me-5' id='5'>
-                    <Project title={title} image={image} app={app} repo={repo} />
-                </div>
-                <div className='col-5 ms-5' id='6'>
-                    <Project title={title} image={image} app={app} repo={repo} />
+                <div className='row justify-content-center pb-5'>
+                    <div className='col-5 me-5' id='5'>
+                        <Project title={title} image={image} app={app} repo={repo} />
+                    </div>
+                    <div className='col-5 ms-5' id='6'>
+                        <Project title={title} image={image} app={app} repo={repo} />
+                    </div>
                 </div>
             </div>
         </div>
