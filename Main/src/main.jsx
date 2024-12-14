c6504fdf741ac4e20b5a5c75bc35cc3ea6fb5c4b
Colton Doling <cmdoling@gmail.com>
2024-12-14 13:55:53 -0500

    removed the index.css import

--- Main/src/main.jsx
+++ Main/src/main.jsx
@@ -2,7 +2,6 @@ import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import './index.css';
 
 import App from './App.jsx';
 import AboutMe from './pages/AboutMe.jsx';
