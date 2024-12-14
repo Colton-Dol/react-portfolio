8cf5860b4c2e48546238e76a70585c83e3c1ea21
Colton Doling <cmdoling@gmail.com>
2024-12-13 22:32:00 -0500

    added a link for X and created an attribution for the old portfolio background

--- Main/src/components/Footer.jsx
+++ Main/src/components/Footer.jsx
@@ -6,10 +6,15 @@ function Footer() {
 const link = 'https://github.com/Colton-Dol';
 
     return (
-        <div className="container text-center">
-            <GitHub link={link} />
-            <a href='https://linkedin.com'><img src='./src/assets/images/In-White-72.png' className='icon p-5'/></a>
-            <a href=''><img src='./src/assets/images/' className='icon p-5' /></a>
+        <div>
+            <div className="container text-center">
+                <GitHub link={link} />
+                <a href='https://linkedin.com' target='_blank'><img src='./src/assets/images/In-White-72.png' className='icon p-5'/></a>
+                <a href='https://x.com/colton_doling' target='_blank'><img src='./src/assets/images/x-logo-black.png' className='icon rounded ms-5 p-2' style={{backgroundColor: '#ffffff', width: 50}}/></a>
+            </div>
+            <div className='text-center ms-3'>
+                <p className='text-light'>Old portfolio background by <a href='https://freepik.com' target='_blank' className='link-underline-opacity-0 link-secondary'>Freepik</a></p>
+            </div>
         </div>
     );
 }
