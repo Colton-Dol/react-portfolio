0072c7e25050b51bc03449e7fdcdea2af993ade1
Colton Doling <cmdoling@gmail.com>
2025-01-19 21:36:52 -0500

    added resume file and linked it in the Resume page

--- Main/src/pages/Resume.jsx
+++ Main/src/pages/Resume.jsx
@@ -4,7 +4,7 @@ function Resume() {
         <div className="text-light ms-5 mt-5">
             <h2 className="ms-5">Resume</h2>
             <br />
-            <p className="ms-5">Download my <a href="./src/assets/" className="link-underline-opacity-0 link-secondary" download>resume</a></p>
+            <p className="ms-5">Download my <a href="./src/assets/Resume.docx" className="link-underline-opacity-0 link-secondary" download>resume</a></p>
             <br />
             <h3 className="ms-5">Front-end Proficiencies</h3>
             <ul className="p-4 ms-5">
