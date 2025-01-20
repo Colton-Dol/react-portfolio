5e140dfd0ba782de823a6bbbaaaf530e35994441
Colton Doling <cmdoling@gmail.com>
2025-01-19 21:35:17 -0500

    updated the bio in the About Me page

--- Main/src/pages/AboutMe.jsx
+++ Main/src/pages/AboutMe.jsx
@@ -4,9 +4,13 @@ function AboutMe() {
         <div className="ms-5 mt-5">
             <h2 className="ms-5 pb-4 text-light">About Me</h2>
             <br />
-            <img src='./src/assets/images/Square-Profile-Picture-2.jpg' className="profpic rounded-circle ms-4" style={{width: 175}} alt="picture of a man with red hair wearing a green shirt, bucket hat, and glasses smiling at the camera" />
+            <img src='./src/assets/images/profile-picture.jpg' className="profpic rounded-circle ms-4" style={{width: 175}} alt="picture of a man with red hair wearing a green shirt, bucket hat, and glasses smiling at the camera" />
             <br />
-            <p className="text-light ms-5 pt-5">Here is a paragraph about my life</p>
+            <p className="text-light ms-5 pt-5">My name is Colton Doling and I am 20 years old. I was born into a Navy family. I am currently a student with edX learning to become a Software Engineer and plan to eventually pursue video game development.</p>
+            <p className="text-light ms-5 pt-5">
+                While I haven't started my career yet, I do have several projects that I've made that you can find in my portfolio. I do my best to be upbeat and keep a positive attitude.
+                I work hard and get the job done to the best of my ability. I enjoy playing D&D with friends, which also helps with improving team work and bonding.
+            </p>
         </div>
     )
 }
