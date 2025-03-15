e7977074a8d2a80435735da86b193784c4f83583
Colton Doling <cmdoling@gmail.com>
2025-03-15 19:49:54 -0400

    updated portfolio projects and profile picture

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,53 +1,37 @@
 // TODO: Implement the Project components in two columns side-by-side. Use props to make each Project different.
 import Project from '../components/Project';
-import prework from '../images/prework.jpg';
-import horiseon from '../images/horiseon-background.jpg';
-import payrole from '../images/payrole-background.jpg';
-import oldPortfolio from '../images/portfolio-background.jpg';
+import candidates from '../images/candidate.png';
 import sunset from '../images/sunset-cloud.jpg';
-import storm from '../images/dark-weather.jpg';
+import adventurersBoard from '../images/empty-guild-board.jpg';
+import treasureBar from '../images/titleScreen.png';
 
 function Portfolio() {
     const project1 = {
-        title: 'Prework Study Guide',
-        image: prework,
-        app: 'https://colton-dol.github.io/prework-study-guide/',
-        repo: 'https://github.com/Colton-Dol/prework-study-guide'
+        title: 'GitHub Candidate Search',
+        image: candidates,
+        app: 'https://github-candidate-search-1.onrender.com/',
+        repo: 'https://github.com/Colton-Dol/github-candidate-search'
     }
 
     const project2 = {
-        title: 'Horiseon',
-        image: horiseon,
-        app: 'https://colton-dol.github.io/horiseon-accessibility-improvement/',
-        repo: 'https://github.com/Colton-Dol/horiseon-accessibility-improvement/'
-    }
-
-    const project3 = {
-        title: 'Payrole Tracker',
-        image: payrole,
-        app: 'https://colton-dol.github.io/payrole-tracker/',
-        repo: 'https://github.com/Colton-Dol/payrole-tracker'
-    }
-
-    const project4 = {
-        title: 'Old Portfolio',
-        image: oldPortfolio,
-        app: 'https://colton-dol.github.io/portfolio/',
-        repo: 'https://github.com/Colton-Dol/portfolio'
-    }
-
-    const project5 = {
         title: `What's the Weather?`,
         image: sunset,
         app: 'https://hieu12-12.github.io/Project-1/',
         repo: 'https://github.com/hieu12-12/Project-1'
     }
 
-    const project6 = {
-        title: 'Weather Dashboard',
-        image: storm,
-        app: 'https://weather-dashboard-7nxn.onrender.com/',
-        repo: 'https://github.com/Colton-Dol/weather-dashboard'
+    const project3 = {
+        title: `Adventurer's Board`,
+        image: adventurersBoard,
+        app: 'https://adventurers-board.onrender.com/',
+        repo: 'https://github.com/tiecaelwaerts/adventurers-board'
+    }
+
+    const project4 = {
+        title: 'Treasure Bar Brawl',
+        image: treasureBar,
+        app: 'https://treasure-bar-brawl.onrender.com/',
+        repo: 'https://github.com/Dkobes/Treasure-Bar-Brawl'
     }
 
     return (
@@ -69,14 +53,6 @@ function Portfolio() {
                     </div>
                     <div className='col-5 ms-5' id='4'>
                         <Project title={project4.title} image={project4.image} app={project4.app} repo={project4.repo} />
-                    </div>
-                </div>
-                <div className='row justify-content-center pb-5'>
-                    <div className='col-5 me-5' id='5'>
-                        <Project title={project5.title} image={project5.image} app={project5.app} repo={project5.repo} />
-                    </div>
-                    <div className='col-5 ms-5' id='6'>
-                        <Project title={project6.title} image={project6.image} app={project6.app} repo={project6.repo} />
                     </div>
                 </div>
             </div>
